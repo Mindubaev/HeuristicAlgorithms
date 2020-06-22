99364e96779bd2fa366585d11905cde47886f434
Mindubaev <a.mindubaev@bk.ru>
2020-06-22 14:31:42 +0300

    hotfix

--- src/main/java/com/mycompany/heuristicalgorithms/Function.java
+++ src/main/java/com/mycompany/heuristicalgorithms/Function.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.simulatedannealing;
+package com.mycompany.heuristicalgorithms;
 
 import java.util.ArrayList;
 import java.util.HashMap;
